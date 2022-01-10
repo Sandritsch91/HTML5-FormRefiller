0cba434570b828240302d1667f70908e1214da33
Sandritsch91 <sandritsch91@gmail.com>
2022-01-10 11:44:39 +0100

    - Added .gitignore - code formatting

--- src/html5FormRefiller.js
+++ src/html5FormRefiller.js
@@ -1,102 +1,106 @@
-(function ( $ ) {
- 
-	$.fn.formRefiller = function( options ) {
-		
-		// Get Settings
-		var settings = $.extend({
-			saveAttribute	 	: "save",
-			storageName			: "myFormData",
-			beforeSave 			: function(instance) {},
-			afterSave 			: function(instance) {},
-			beforeRefill 		: function(instance) {},
-			afterRefill 		: function(instance) {}
-		}, options );
+(function($) {
 
-		
-		// Instance
-		var instance = this;
+    $.fn.formRefiller = function(options) {
 
-		
-		/**
-		 * Function gets the data from localStorage and puts it 
-		 * in the form
-		 */
-		this.refillData = function() {
-			
-			// get the Data
-			var data = localStorage.getItem(settings.storageName);
-			data = $.parseJSON(data);
-			
-			$.each( data, function() {
-				var item = this;
-				var element = $("[name='"+this.name+"']");
-
-				// checkbox
-				if ( element.attr("type") === "checkbox" ) {
-					element.prop("checked", true);
-				}
-				// radio - buttons
-				else if ( element.attr("type") === "radio" ) {
-					//element.
-					var radio = element = $("[value='"+this.value+"']");
-					radio.prop("checked", true)
-				}
-				// select / select multiple
-				else if ( element.is("select")  ) {
-					var option = element.find("option[value='"+this.value+"']");
-					option.prop("selected", true);
-				}
-				// input / texteara
-				else {
-					element.val(item.value);					
-				}
-			});
-		}
+        // Get Settings
+        var settings = $.extend({
+            saveAttribute: "save",
+            storageName: "myFormData",
+            beforeSave: function(instance) {
+            },
+            afterSave: function(instance) {
+            },
+            beforeRefill: function(instance) {
+            },
+            afterRefill: function(instance) {
+            }
+        }, options);
 
 
-		/**
-		 * Function gets the data from the form and puts it
-		 * in localStorage
-		 */
-		this.saveData = function() {
-
-			// Create JSON-Format
-			var toSave = $("["+settings.saveAttribute+"]").serializeArray();
-			toSave = JSON.stringify(toSave);
-
-			// Save to localStorage
-			localStorage.setItem(settings.storageName, toSave);
-		}
+        // Instance
+        var instance = this;
 
 
-		// Bind onSubmit-Event
-		this.submit(function() {
-			
-			// beforeSave callback-function
-			settings.beforeSave(instance);
-			
-			// Save the data
-			instance.saveData();
+        /**
+         * Function gets the data from localStorage and puts it
+         * in the form
+         */
+        this.refillData = function() {
 
-			// afterSave callback-function
-			settings.afterSave(instance);
+            // get the Data
+            var data = localStorage.getItem(settings.storageName);
+            data = $.parseJSON(data);
 
-			// submit form
-			return true;
-		});
+            $.each(data, function() {
+                var item = this;
+                var element = $("[name='" + this.name + "']");
+
+                // checkbox
+                if ( element.attr("type") === "checkbox" ) {
+                    element.prop("checked", true);
+                }
+                // radio - buttons
+                else if ( element.attr("type") === "radio" ) {
+                    //element.
+                    var radio = element = $("[value='" + this.value + "']");
+                    radio.prop("checked", true)
+                }
+                // select / select multiple
+                else if ( element.is("select") ) {
+                    var option = element.find("option[value='" + this.value + "']");
+                    option.prop("selected", true);
+                }
+                // input / texteara
+                else {
+                    element.val(item.value);
+                }
+            });
+        }
 
 
-		// beforeRefill callback-function
-		settings.beforeRefill(this);
+        /**
+         * Function gets the data from the form and puts it
+         * in localStorage
+         */
+        this.saveData = function() {
 
-		// get data from localStorage
-		this.refillData();
+            // Create JSON-Format
+            var toSave = $("[" + settings.saveAttribute + "]").serializeArray();
+            toSave = JSON.stringify(toSave);
 
-		// afterRefill callback-function
-		settings.afterRefill(this);
-		
-		return this;
-		
-	};
- 
-}( jQuery ));
+            // Save to localStorage
+            localStorage.setItem(settings.storageName, toSave);
+        }
+
+
+        // Bind onSubmit-Event
+        this.submit(function() {
+
+            // beforeSave callback-function
+            settings.beforeSave(instance);
+
+            // Save the data
+            instance.saveData();
+
+            // afterSave callback-function
+            settings.afterSave(instance);
+
+            // submit form
+            return true;
+        });
+
+
+        // beforeRefill callback-function
+        settings.beforeRefill(this);
+
+        // get data from localStorage
+        this.refillData();
+
+        // afterRefill callback-function
+        settings.afterRefill(this);
+
+        return this;
+
+    };
+
+}(jQuery));
